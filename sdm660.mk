ae8bbf38f5a14c42f69e0991ae1779ac4c239760
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    sdm660-common: Remove packages at build time

--- sdm660.mk
+++ sdm660.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
